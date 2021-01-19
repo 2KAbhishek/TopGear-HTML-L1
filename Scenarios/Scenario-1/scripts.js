9786688fd761fe3ceb1e52feebebffa45f2757cd
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-01-19 21:26:03 +0530

    Remove obsolete script

--- Scenarios/Scenario-1/scripts.js
+++
@@ -1,5 +0,0 @@
-function validateStock() {
-    var quantity = document.forms["stock"]["qty"].value;
-    quantity = parseInt(quantity);
-    if (quantity < 10 || quantity > 100 || quantity % 10 !== 0) alert("Can only buy in blocks of 10");
-}
